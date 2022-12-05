45c1b946a0281b7d7e13da8380ce60a03ca8dab0
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2022-12-05 10:13:15 +0100

    Set the git identity before patching

--- src/setup-ghidra-source.cmake
+++ src/setup-ghidra-source.cmake
@@ -31,7 +31,9 @@ set(sleigh_ADDITIONAL_PATCHES "" CACHE STRING
 
 # pinned stable patches list
 set(ghidra_patches
-  PATCH_COMMAND "${GIT_EXECUTABLE}" am --ignore-space-change --ignore-whitespace --no-gpg-sign
+  PATCH_COMMAND "${GIT_EXECUTABLE}" config user.name "CMake" &&
+  "${GIT_EXECUTABLE}" config user.email "patch@cmake" &&
+  "${GIT_EXECUTABLE}" am --ignore-space-change --ignore-whitespace --no-gpg-sign
   "${CMAKE_CURRENT_LIST_DIR}/patches/stable/0001-Small-improvements-to-C-decompiler-testing-from-CLI.patch"
   "${CMAKE_CURRENT_LIST_DIR}/patches/stable/0002-Add-include-guards-to-decompiler-C-headers.patch"
   "${CMAKE_CURRENT_LIST_DIR}/patches/stable/0003-Fix-UBSAN-errors-in-decompiler.patch"
@@ -48,7 +50,9 @@ if("${sleigh_RELEASE_TYPE}" STREQUAL "HEAD")
   set(ghidra_git_tag "${ghidra_head_git_tag}")
   set(ghidra_shallow FALSE)
   set(ghidra_patches
-    PATCH_COMMAND "${GIT_EXECUTABLE}" am --ignore-space-change --ignore-whitespace --no-gpg-sign
+    PATCH_COMMAND PATCH_COMMAND "${GIT_EXECUTABLE}" config user.name "CMake" &&
+    "${GIT_EXECUTABLE}" config user.email "patch@cmake" &&
+    "${GIT_EXECUTABLE}" am --ignore-space-change --ignore-whitespace --no-gpg-sign
     "${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0001-Small-improvements-to-C-decompiler-testing-from-CLI.patch"
     "${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0002-Add-include-guards-to-decompiler-C-headers.patch"
     "${CMAKE_CURRENT_LIST_DIR}/patches/HEAD/0003-Fix-UBSAN-errors-in-decompiler.patch"
